7a46fef6ec893a4db89c35f14ca4e3c0fdd56c99
DiegoVeiga <diegoroberto27@hotmail.com>
2025-04-29 07:17:46 -0300

    fix: remove comments

--- src/database/db.ts
+++ src/database/db.ts
@@ -1,11 +1,11 @@
 import mongoose from 'mongoose';
 
 const options = {
-  autoIndex: false, // Don't build indexes
-  maxPoolSize: 10, // Maintain up to 10 socket connections
-  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
-  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
-  family: 4, // Use IPv4, skip trying IPv6
+  autoIndex: false,
+  maxPoolSize: 10,
+  serverSelectionTimeoutMS: 5000,
+  socketTimeoutMS: 45000,
+  family: 4,
 };
 
 export const connectDatabase = () =>
